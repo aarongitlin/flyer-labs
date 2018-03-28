6521bdf04de35a86fb93262f54248ee37f7c5780
agitlin-pp <agitlin@paperlesspost.com>
2018-03-28 01:57:01 -0400

    Automated commit at 2018-03-28 05:57:01 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,35 @@
 // This is where it all goes :)
+
+var containerWidth = $(".blocks-list").width();
+var spanHeight = $(this).find("span").height();
+$(".basic-info__title").width((containerWidth * .85)).height(114);
+$(".text-block__title, .guestlist__title").width((containerWidth * .65)).height(64);
+
+$(".basic-info__title").textfill({
+  maxFontPixels: 112
+});
+$(".text-block__title").textfill({
+  maxFontPixels: 64
+});
+$(".guestlist__title").textfill({
+  maxFontPixels: 64
+});
+
+$( window ).bind("resize", function(){
+  var containerWidth = $(".basic-info").width();
+  var spanHeight = $(this).find("span").height();
+  $(".basic-info__title").width((containerWidth * .85));
+  $(".text-block__title, .guestlist__title").width((containerWidth * .65));
+});
+
+$( window ).bind("resize", function(){
+  $(".basic-info__title").textfill({
+    maxFontPixels: 112
+  });
+  $(".text-block__title").textfill({
+    maxFontPixels: 64
+  });
+  $(".guestlist__title").textfill({
+    maxFontPixels: 64
+  });
+});
