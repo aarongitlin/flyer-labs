76f33b247f66fbfdc396112a06ace0c67c5f016e
agitlin-pp <agitlin@paperlesspost.com>
2018-04-13 00:43:37 -0400

    tidied up some files, updated phase 3

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,44 +1,45 @@
-
-// Get and set widths for the titles within their containers
-var containerWidth = $(".blocks-list").width();
-$(".basic-info__title").width((containerWidth * .85)).height(114);
-$(".text-block__title, .guestlist__title").width((containerWidth * .65)).height(64);
-
-// Initialize textfill with min and max font sizes
-$(".basic-info__title").textfill({
-  maxFontPixels: 112
-});
-$(".text-block__title").textfill({
-  maxFontPixels: 64
-});
-$(".guestlist__title").textfill({
-  maxFontPixels: 64
-});
-
-var spanHeight = $(".basic-info__title > span").height();
-$(".basic-info__title").css(
-  "margin-bottom", ( spanHeight - $(".basic-info__title").height() + 32 )
-);
-
-// Bind the same width calculations to resize events, so it's calcualted on the fly
-$( window ).bind("resize", function(){
-  var containerWidth = $(".basic-info").width();
-  $(".basic-info__title").width((containerWidth * .85));
-  $(".text-block__title, .guestlist__title").width((containerWidth * .65));
-
-// Bind the textfill plugin on resize as well, using the widths calculated above
-  $(".basic-info__title").textfill({
-    maxFontPixels: 112
-  });
-  $(".text-block__title").textfill({
-    maxFontPixels: 64
-  });
-  $(".guestlist__title").textfill({
-    maxFontPixels: 64
-  });
-
-  var spanHeight = $(".basic-info__title > span").height();
-  $(".basic-info__title").css(
-    "margin-bottom", ( spanHeight - $(".basic-info__title").height() + 32 )
-  );
-});
+//
+// // Get and set widths for the titles within their containers
+// var containerWidth = $(".blocks-list").width();
+// $(".basic-info__title").width((containerWidth * .85)).height(115);
+// $(".text-block__title, .guestlist__title").width((containerWidth * .65)).height(64);
+//
+// // Initialize textfill with min and max font sizes
+// $(".basic-info__title").textfill({
+//   maxFontPixels: 88
+// });
+// $(".text-block__title").textfill({
+//   maxFontPixels: 64
+// });
+// $(".guestlist__title").textfill({
+//   maxFontPixels: 64
+// });
+//
+// var containerHeight = 115;
+// var textHeight = $(".basic-info__title > span").height();
+// var newMargin = textHeight - containerHeight;
+// $(".basic-info__title").css( "margin-bottom", newMargin );
+//
+// // Bind the same width calculations to resize events, so it's calcualted on the fly
+// $( window ).bind("resize", function(){
+//   var containerWidth = $(".basic-info").width();
+//   $(".basic-info__title").width((containerWidth * .85));
+//   $(".text-block__title, .guestlist__title").width((containerWidth * .65));
+//
+// // Bind the textfill plugin on resize as well, using the widths calculated above
+//   $(".basic-info__title").textfill({
+//     maxFontPixels: 112
+//   });
+//   $(".text-block__title").textfill({
+//     maxFontPixels: 64
+//   });
+//   $(".guestlist__title").textfill({
+//     maxFontPixels: 64
+//   });
+//
+//   var containerHeight = 115;
+//   var textHeight = $(".basic-info__title > span").height();
+//   var minMargin = 44;
+//   var newMargin = textHeight - containerHeight;
+//   $(".basic-info__title").css( "margin-bottom", newMargin );
+// });
